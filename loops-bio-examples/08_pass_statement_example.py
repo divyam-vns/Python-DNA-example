e07bdc319a65b69ceccb57547668eb64fcd87a95
divyam-vns <129139215+divyam-vns@users.noreply.github.com>
2025-11-14 10:19:55 -0800

    Update 08_pass_statement_example.py

--- loops-bio-examples/08_pass_statement_example.py
+++ loops-bio-examples/08_pass_statement_example.py
@@ -1,6 +1,11 @@
+# Demonstrate use of pass.
 motif = "ATG"
 dna = "GGGATGCCC"
+
 if motif not in dna:
-    pass
+    pass  # placeholder, code will be inserted later
 else:
     print("Motif found!")
+
+# output- 
+# Motif found!
