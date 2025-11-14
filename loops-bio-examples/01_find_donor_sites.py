e32a40197664a860ecea7d03e25f367cc6239428
divyam-vns <129139215+divyam-vns@users.noreply.github.com>
2025-11-14 09:58:06 -0800

    Update 01_find_donor_sites.py

--- loops-bio-examples/01_find_donor_sites.py
+++ loops-bio-examples/01_find_donor_sites.py
@@ -1,5 +1,9 @@
+# Example DNA sequence
 dna = "acgtgagtccgtatgtttaggtttgt"
+
+# Find all "gt" donor splice sites
 pos = dna.find("gt")
+
 while pos > -1:
     print("Found donor site (gt) at position:", pos)
     pos = dna.find("gt", pos + 1)
