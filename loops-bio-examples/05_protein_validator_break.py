cc0adb51ce89820c32dbd5aa21340bb035d7feff
divyam-vns <129139215+divyam-vns@users.noreply.github.com>
2025-11-14 11:01:07 -0800

    Update 05_protein_validator_break.py

--- loops-bio-examples/05_protein_validator_break.py
+++ loops-bio-examples/05_protein_validator_break.py
@@ -1,6 +1,8 @@
+# Problem- suppose we are only interested in finding if a protein sequence is valid, not where are all the invalid characters in the sequence.
+
 # Stop at the first invalid amino acid.
 protein = "MRTGWUASDJKLQWERUITYPAAJCH"
-valid_aas = "ARNDCEQGHILKMFPSTWYV"
+valid_aas = "ARNDCEQGHILKMFPSTWYV"  # 20 letters to represent 20 amimo acids.
 for i in range(len(protein)):
     if protein[i] not in valid_aas:
         print("Not a valid protein sequence.")
