f743cc4df8b77911449931c41d3b28400acfb5c7
divyam-vns <129139215+divyam-vns@users.noreply.github.com>
2025-11-14 10:13:19 -0800

    Update 01_find_donor_sites.py

--- loops-bio-examples/01_find_donor_sites.py
+++ loops-bio-examples/01_find_donor_sites.py
@@ -1,3 +1,4 @@
+# Scan DNA for canonical donor splice sites ("gt").
 # Example DNA sequence
 dna = "acgtgagtccgtatgtttaggtttgt"
 
@@ -7,3 +8,11 @@ pos = dna.find("gt")
 while pos > -1:
     print("Found donor site (gt) at position:", pos)
     pos = dna.find("gt", pos + 1)
+
+# output 
+# Found donor site (gt) at position: 2
+# Found donor site (gt) at position: 6
+# Found donor site (gt) at position: 10
+# Found donor site (gt) at position: 14
+# Found donor site (gt) at position: 20
+# Found donor site (gt) at position: 24
