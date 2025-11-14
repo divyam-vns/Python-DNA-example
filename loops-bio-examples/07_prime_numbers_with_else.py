0d6f724b02d69d789028b9f199a6ed2bb8eac124
divyam-vns <129139215+divyam-vns@users.noreply.github.com>
2025-11-14 10:18:09 -0800

    Update 07_prime_numbers_with_else.py

--- loops-bio-examples/07_prime_numbers_with_else.py
+++ loops-bio-examples/07_prime_numbers_with_else.py
@@ -1,3 +1,5 @@
+# Loop with inner for + else.
+
 N = 10
 for y in range(2, N):
     for x in range(2, y):
@@ -6,3 +8,13 @@ for y in range(2, N):
             break
     else:
         print(y, "is a prime number")
+
+# Output:
+# 2 is a prime number
+# 3 is a prime number
+# 4 = 2 * 2
+# 5 is a prime number
+# 6 = 2 * 3
+# 7 is a prime number
+# 8 = 2 * 4
+# 9 = 3 * 3
