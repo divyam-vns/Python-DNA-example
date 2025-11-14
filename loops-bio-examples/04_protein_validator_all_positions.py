04c5d448f5001d0c073f10501946eb75c0c9ea4a
divyam-vns <129139215+divyam-vns@users.noreply.github.com>
2025-11-14 10:04:55 -0800

    Update 04_protein_validator_all_positions.py

--- loops-bio-examples/04_protein_validator_all_positions.py
+++ loops-bio-examples/04_protein_validator_all_positions.py
@@ -1,5 +1,12 @@
-protein = "MRTGWUASDJKLQWERUITYPAAJCH"
+protein = "MRTGWUASDJKLQWERUITYPAAJCH"  # contains invalid U, J
+
 valid_aas = "ARNDCEQGHILKMFPSTWYV"
+
 for i in range(len(protein)):
     if protein[i] not in valid_aas:
         print("Invalid amino acid", protein[i], "at position", i)
+# output 
+# Invalid amino acid U at position 5
+# Invalid amino acid J at position 9
+# Invalid amino acid U at position 16
+# Invalid amino acid J at position 23
