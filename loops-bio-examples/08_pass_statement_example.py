2347b386ea3cb2ba72ac3453e542aa74629c563a
divyam-vns <129139215+divyam-vns@users.noreply.github.com>
2025-11-14 11:18:54 -0800

    Update 08_pass_statement_example.py

--- loops-bio-examples/08_pass_statement_example.py
+++ loops-bio-examples/08_pass_statement_example.py
@@ -9,3 +9,7 @@ else:
 
 # output- 
 # Motif found!
+
+
+# pass statement is a placeholder, it does nothing. it is used when a statement required syntactically but you don't want any command or code to execute. 
+# sometimes you can use the pass statement also when you didn’t yet write the codefor a particular situation but you need the placeholder so thjat the syntax of the rest of your program is correct.
